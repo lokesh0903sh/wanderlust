3caecdbc2104e81436c4bec42aa0639c7db2ffa0
lokesh0903sh <lokesh0903sh@gmail.com>
2024-07-17 02:27:18 +0530

    update

--- models/listing.js
+++ models/listing.js
@@ -19,8 +19,8 @@ let listingSchema = new mongoose.Schema({
     },
     image:{
         type: String,
-        default: "https://unsplash.com/photos/a-person-climbing-up-the-side-of-a-large-rock-E76-vNiW3ps",
-        set: (v)=> v===""?"https://unsplash.com/photos/a-person-climbing-up-the-side-of-a-large-rock-E76-vNiW3ps":v,
+        default: "https://media.istockphoto.com/id/487619266/photo/mirrors-lake.jpg?s=1024x1024&w=is&k=20&c=1EWCx9k4m8oPoMYNKSmCk0zoYDZEcwZWv_9P2CkZ_mA=",
+        set: (v)=> v===""?"https://media.istockphoto.com/id/487619266/photo/mirrors-lake.jpg?s=1024x1024&w=is&k=20&c=1EWCx9k4m8oPoMYNKSmCk0zoYDZEcwZWv_9P2CkZ_mA=":v,
         required: true
     },
     price:{
